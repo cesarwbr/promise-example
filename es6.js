18c9a00db78ff88a2be5f36da7304df940d887c7
Cesar William <cesarwbr@gmail.com>
2017-01-20 09:30:10 -0200

    Update es6 and array files

---
+++ es6.js
@@ -0,0 +1,73 @@
+// let team = 'bulls'
+
+// while(true) {
+//   let team = 'heat'
+//   console.log(team)
+//   break
+// }
+
+// console.log(team)
+
+// const PI = Math.PI
+
+// const moment = require('moment')
+
+// class Basketball extends Sport {
+//   constructor () {
+//     super()
+//     this.type = 'basketball'
+//   }
+// }
+
+// const sport = new Basketball()
+// sport.plays('basketball')
+
+// class Sport {
+//   constructor () {
+//     this.type = 'sport'
+//   }
+
+//   plays (play) {
+//     console.log(this.type, play)
+//   }
+// }
+
+// const sports = ['basketball', 'soccer'].filter((sport) => sport !== 'basketball')
+
+// console.log(sports)
+
+// const sport = {
+//   type: 'basketball',
+//   showType: function () {
+//     setTimeout(() => {
+//       console.log(this.type)
+//     }, 1000)
+//   }
+// }
+
+// sport.showType()
+
+// const sport = 'basketball'
+// console.log('My favorite sport is ' + sport)
+
+// const sport1 = 'basketball'
+// const sport2 = 'soccer'
+
+// const sports = {sport1, sport2}
+
+// const sport = {name: 'basketball', team: 'bulls'}
+
+// const {team, name} = sport
+// console.log(name, team)
+
+// function sport (type = 'basketball') {
+//   console.log(type)
+// }
+
+// sport('alguma coisa')
+
+// function sports (...types) {
+//   console.log(name, types)
+// }
+
+// sports('my sports', 'basketball', 'soccer')
